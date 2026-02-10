f1087be9b9fabb7910ffe7048f05fb587b2ee5ed
Dominik <dominik.reinartz@proton.me>
2026-02-10 23:24:30 +0100

    flash script now overwrites old files, fixed it ignoring everything before the last underscore of a file name

--- flash.lua
+++ flash.lua
@@ -1,9 +1,13 @@
 local dirs = { ... }
 
 for _, dir in ipairs(dirs) do
-    local finalPart = dir:match([[[%w\.]+$]])
+    local finalPart = dir:match([[[%w\._]+$]])
     if finalPart then
         local isLib = dir:match([[/libs/]]) ~= nil or dir:match([[/lib/]]) ~= nil
-        fs.copy("/disk/contents/" .. dir, "/" .. (isLib and "libs/" or "") .. finalPart)
+        local target = "/" .. (isLib and "libs/" or "") .. finalPart
+        if fs.exists(target) then
+            fs.delete(target)
+        end
+        fs.copy("/disk/contents/" .. dir, target)
     end
 end

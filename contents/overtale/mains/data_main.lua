01f3b2559fcca04a3a4cb85672f9f23961e5f18c
Dominik <dominik.reinartz@proton.me>
2026-02-11 00:34:31 +0100

    changed data_main setting ids

--- contents/overtale/mains/data_main.lua
+++ contents/overtale/mains/data_main.lua
@@ -1,13 +1,13 @@
 local data = require(".libs.data")
 
-local idOpticalSensorLong = settings.get("idOpticalSensorLong")
-local idOpticalSensorFluid = settings.get("idOpticalSensorFluid")
+local idOpticalSensorLong = settings.get("id_optical_sensor_long")
+local idOpticalSensorFluid = settings.get("id_optical_sensor_fluid")
 if idOpticalSensorLong == nil or idOpticalSensorFluid == nil then
     error("Not all optical sensor IDs were specified using the settings api.")
 end
 
-local opticalSensorLong = peripheral.wrap("idOpticalSensorLong")
-local opticalSensorFluid = peripheral.wrap("idOpticalSensorFluid")
+local opticalSensorLong = peripheral.wrap(idOpticalSensorLong)
+local opticalSensorFluid = peripheral.wrap(idOpticalSensorFluid)
 if opticalSensorLong == nil or opticalSensorFluid == nil then
     error("Couldn't find every specified optical sensor.")
 end

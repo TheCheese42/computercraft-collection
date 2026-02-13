c7ba253f567d6f5b0ae40aa2fc82bfbcd415473c
Dominik <dominik.reinartz@proton.me>
2026-02-13 17:55:28 +0100

    Started physics controller

---
+++ contents/overtale/libs/physics.lua
@@ -0,0 +1,44 @@
+local GRAVITY_ACCELERATION <const> = 10
+local THRUSTER_FORCE <const> = 600000
+
+local function getMass()
+    return ship.getMass()
+end
+
+local function getLocation()
+    return ship.getWorldspacePosition()
+end
+
+local function getAngle()
+    local _, yaw, _ = ship.getQuaternion():toEuler()
+    return math.pi + (yaw < 0 and math.pi + yaw or -math.pi + yaw)
+end
+
+local function getWeight()
+    return getMass() * GRAVITY_ACCELERATION
+end
+
+local function calcThrusterForce(thrusterForceMultiplier)
+    return THRUSTER_FORCE * thrusterForceMultiplier
+end
+
+local function multiplierForRedstone(value, level, maxLevel)
+    return level * 1 / (maxLevel or 15) * value
+end
+
+local function redstoneForTargetValue(maxValue, targetValue, maxLevel)
+    return (maxLevel or 15) * targetValue / maxValue
+end
+
+local function distributeRedstoneOverAmount(level, amount)
+    local distribution = {}
+    for i = amount, 1, -1 do
+        local levelForObject = level / i
+        if levelForObject % 1 ~= 0 then
+            levelForObject = math.ceil(levelForObject)
+        end
+        level = level - levelForObject
+        table.insert(distribution, levelForObject)
+    end
+    return distribution
+end
---
+++ contents/overtale/mains/controls_main.lua
@@ -0,0 +1,50 @@
+local THRUSTER_FORCE_MULTIPLIER <const> = 1.2
+local MODULE_COUNT <const> = 4
+local THRUSTERS_PER_MODULE <const> = 8
+
+local physics = require(".libs.physics")
+
+local function getForcePerModuleToHold()
+    return physics.getWeight() / MODULE_COUNT
+end
+
+local function calcModuleForce()
+    return physics.calcThrusterForce(THRUSTER_FORCE_MULTIPLIER) * THRUSTERS_PER_MODULE
+end
+
+local function redstonePerModuleToHold()
+    local maxForcePerThruster = physics.calcThrusterForce(THRUSTER_FORCE_MULTIPLIER)
+    local maxForcePerModule = maxForcePerThruster * THRUSTERS_PER_MODULE
+    return physics.redstoneForTargetValue(calcModuleForce(), getForcePerModuleToHold(), 15 * THRUSTERS_PER_MODULE)
+end
+
+local function applyRedstoneDistribution(distribution)
+    local relay1, relay2 = peripheral.find("redstone_relay")
+    for i, strength in ipairs(distribution) do
+        if i == 1 then
+            relay1.setAnalogOutput("front", strength)
+        elseif i == 2 then
+            relay2.setAnalogOutput("front", strength)
+        elseif i == 3 then
+            relay1.setAnalogOutput("back", strength)
+        elseif i == 4 then
+            relay2.setAnalogOutput("back", strength)
+        elseif i == 5 then
+            relay1.setAnalogOutput("bottom", strength)
+        elseif i == 6 then
+            relay2.setAnalogOutput("bottom", strength)
+        elseif i == 7 then
+            relay1.setAnalogOutput("left", strength)
+        elseif i == 8 then
+            relay2.setAnalogOutput("right", strength)
+        end
+    end
+end
+
+while true do
+    peripheral.find(
+        "modem",
+        function(name, modem) if modem.isWireless() then return rednet.open(name) end end
+    )
+    local command = rednet.receive("control_tick")
+end
